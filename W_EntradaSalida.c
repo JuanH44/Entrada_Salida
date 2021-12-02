7247c0998c5135a8f8c0dea768ff4ff02a10a809
JuanHdez <juanh7522@gmail.com>
2021-12-02 03:06:02 -0600

    menu

--- W_EntradaSalida.c
+++ W_EntradaSalida.c
@@ -9,23 +9,69 @@
 int crear(char* );
 HANDLE abrir(char* );
 int escribir(HANDLE);
+void menu(int *n);
 
 
 int main(int argc, char const *argv[]) {
+  int n;
 
-  //crear("hola.txt");
-  HANDLE hFile = abrir("hola.txt");
-
-  escribir(hFile);
-
-  
+  do{
+    menu(&n);
+  }while(n!=0);
 
   return 0;
 }
 
+void menu(int* n){
+  system("cls");
+  printf(
+    "\n\tSeleccione una opcion:"
+    "\n\t 1. Crear"
+    "\n\t 2. Leer"
+    "\n\t 3. Escribir"
+    "\n\t 4. Renombrar"
+    "\n\t 5. Agregar al final"
+    "\n\t Eleccion: ");
+    scanf("%d", n);
+  switch (*n) {
+    case 1:
+      crear("hola3.txt");
+    break;
+    case 2:
+      //abrir
+    break;
+    case 3:
+      //leer
+    break;
+    case 4:
+      //escribir
+    break;
+    case 5:
+     //renombrar
+    break;
+    case 6:
+     //agregar al final
+    break;
+    case 7:
+     //cerrar
+    break;
+    default:
+      printf("\n\tHasta luego!");
+
+  }
+  printf("\n\t");
+  system("pause");
+  fflush(stdin);
+}
+
 int crear(char* dir){
+    char nombre[32];
+    fflush(stdin);
+    printf("\n\tIngrese el nombre del archivo:\n\t");
+    scanf("%[^\n]",nombre);
+
     HANDLE h = CreateFile(
-        dir,
+        nombre,
         GENERIC_READ | GENERIC_WRITE,
         0,
         NULL,
@@ -35,17 +81,13 @@ int crear(char* dir){
 
     if (h == INVALID_HANDLE_VALUE) {
         DWORD err = GetLastError();
-        printf("err %d\n", err);
+        printf("\n\t[!] A ocurrido un error al crear el archivo %lu\n", err);
         return 1;
     }
-    printf("%p\n", h);
+    printf("\n\t[+] Se ha creado el archivo satisfatoriamente\n");
 
-    //int fd = _open_osfhandle(h, 0);
-
-    //printf("fd %d\n", fd);
-
+    CloseHandle(h);
     return 0;
-
 }
 
 
